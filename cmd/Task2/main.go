aca2d3e6d87b76306e64b801a1df237f039d1946
YashP77 <yash_patel1998@live.com>
2025-12-23 10:41:09 +0000

    003: Task 2 Update 1

--- cmd/Task2/main.go
+++ cmd/Task2/main.go
@@ -12,22 +12,26 @@ import (
 
 func main() {
 
+	// Create context and add TraceID
 	ctx := context.Background()
 	ctx = internal.WithTraceID(ctx, uuid.NewString())
-	internal.LogWithTrace(ctx, "Starting application")
 
+	// Create signal
+	internal.LogWithTrace(ctx, "Starting application")
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
 
+	// Create flags
 	message, userID := internal.CliFlags(ctx)
 	file := internal.OpenFile(ctx)
 	defer file.Close()
 
+	// Write and read logic
 	internal.WriteToFile(ctx, *file, message, userID)
 	internal.ReadLastTen(ctx)
 
+	// Shutdown
 	internal.LogWithTrace(ctx, "Application running. Press CTRL+C to exit")
-
 	sig := <-sigChan
 	internal.LogWithTrace(ctx, "Received signal: "+sig.String())
 	internal.LogWithTrace(ctx, "Shutting down gracefully")
